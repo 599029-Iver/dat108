24f406a223e383e636a643f76c6b07123501ba2c
599029-Iver <89256936+599029-Iver@users.noreply.github.com>
2022-10-11 13:49:56 +0200

    oblig3

---
+++ Obligatorisk3/Oppgave1/Oppgave1/src/main/java/Oblig3/Oppgave1/Oppgave1Application.java
@@ -0,0 +1,13 @@
+package Oblig3.Oppgave1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oppgave1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oppgave1Application.class, args);
+	}
+
+}
---
+++ Obligatorisk3/Oppgave1/Oppgave1/src/test/java/Oblig3/Oppgave1/Oppgave1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Oblig3.Oppgave1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oppgave1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
